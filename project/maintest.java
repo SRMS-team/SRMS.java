fe6e6ed7fd0420794d5a98af2de05d9413475c72
rawan11-it <443200031@qu.edu.sa>
2025-12-17 11:17:37 +0300

    Update maintest.java

--- project/maintest.java
+++ project/maintest.java
@@ -2,56 +2,94 @@ package project;
 
 public class maintest {
     public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
         Register reg = new Register();
         RecordManager rm = new RecordManager();
 
-        Student s1 = new Student("rawan", 21, "S443", "443@qu.edu.sa", "0501830446", "IT", 3, 3.5);
-        Student s2 = new Student("deem", 22, "S444", "444@qu.edu.sa", "0553344989", "CS", 3, 3.8);
-        Instructor inst = new Instructor("Dr.Jawaher", 32, "I01", "inst@qu.edu.sa", "05555444", "INS100",
-                "Software Engineering");
-        Course oop = new Course("OOP", "CS201", 3, inst, "CS");
+        System.out.println("**Welcome to University Management System **");
 
-        reg.addStudents(s1);
-        reg.addStudents(s2);
-        reg.addCourse(oop);
+        // 1. Input Student Data
+        System.out.println("\n[1] Enter Student Details:");
+        System.out.print("Name: "); String sName = sc.nextLine();
+        System.out.print("Age: "); int sAge = sc.nextInt(); sc.nextLine(); 
+        System.out.print("ID: "); String sID = sc.nextLine();
+        System.out.print("Email: "); String sEmail = sc.nextLine();
+        System.out.print("Phone: "); String sPhone = sc.nextLine();
+        System.out.print("Department: "); String sDept = sc.nextLine();
+        System.out.print("Year: "); int sYear = sc.nextInt();
+        System.out.print("GPA: "); double sGPA = sc.nextDouble(); sc.nextLine();
 
-        reg.enrollStudent(s1, oop);
-        reg.enrollStudent(s2, oop);
+        Student student = new Student(sName, sAge, sID, sEmail, sPhone, sDept, sYear, sGPA);
 
-        reg.showLog();
+     // Instructor data
+        System.out.println("\n[2] Enter Instructor Details:");
+        System.out.print("Name: "); String iName = sc.nextLine();
+        System.out.print("Age: "); int iAge = sc.nextInt(); sc.nextLine();
+        System.out.print("ID: "); String iID = sc.nextLine();
+        System.out.print("Email: "); String iEmail = sc.nextLine();
+        System.out.print("Phone: "); String iPhone = sc.nextLine();
+        System.out.print("Instructor ID: "); String iRefID = sc.nextLine();
+        System.out.print("Specialization: "); String iSpec = sc.nextLine();
 
-        rm.addStudent(s1);
-        rm.addStudent(s2);
+        Instructor instructor = new Instructor(iName, iAge, iID, iEmail, iPhone, iRefID, iSpec);
 
+        // 3. Input Course Data
+        System.out.println("\n[3] Enter Course Details:");
+        System.out.print("Course Name: "); String cName = sc.nextLine();
+        System.out.print("Course ID: "); String cID = sc.nextLine();
+        System.out.print("Credit Hours: "); int cHours = sc.nextInt(); sc.nextLine();
+
+        Course course = new Course(cName, cID, cHours, instructor, sDept);
+
+    
+        reg.addStudents(student);
+        reg.addCourse(course);
+        reg.enrollStudent(student, course);
+        
+        rm.addStudent(student);
         rm.saveStudentsToFile("students.txt");
-        rm.loadStudentsFromFile("students.txt");
+
+        System.out.println("\n Processing Data using Threads ");
+
 
         Thread t1 = new Thread(() -> {
-            System.out.println("\n Thread 1: Student Info ");
-            s1.displayInfo();
+            synchronized (System.out) {
+                System.out.println("\n Thread 1: Displaying Student Info");
+                student.displayInfo();
+            }
         });
 
         Thread t2 = new Thread(() -> {
-            System.out.println("\n Thread 2: Course Info ");
-            oop.displayInfo();
+            synchronized (System.out) {
+                System.out.println("\n Thread 2: Displaying Course Info");
+                course.displayInfo();
+            }
         });
 
         Thread t3 = new Thread(() -> {
-            System.out.println("\n Thread 3: Instructor Info ");
-            inst.displayInfo();
+            synchronized (System.out) {
+                System.out.println("\n Thread 3: Displaying Instructor Info");
+               instructor.displayInfo();
+            }
         });
+
+     
+        t1.start();
+        t2.start();
+        t3.start();
+
+  
         try {
-            t1.start();
             t1.join();
-
-            t2.start();
             t2.join();
-
-            t3.start();
             t3.join();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("Thread interrupted.");
         }
-    }
 
-}
+        System.out.println("\n Operation Completed Successfully ");
+        sc.close();
+        
+    }}
+        
